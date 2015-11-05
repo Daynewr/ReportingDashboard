2463b3292bc0d2e060dfd10acffb0930412bb1ba
Dayne <dayne@pilgrimconsulting.com>
2015-11-05 14:30:24 -0600

    testing errors

--- dashboard/db/dbPDO.php
+++ dashboard/db/dbPDO.php
@@ -1,7 +1,7 @@
 <?php
 
   error_reporting(E_ALL);
-  ini_set('display_errors', 0);
+  ini_set('display_errors', 1);
   //setup new PDO class
   if(!$db) {
     $db = new PDO('mysql:host=localhost;dbname=spyrgdb;charset=utf8', 'root', '');
