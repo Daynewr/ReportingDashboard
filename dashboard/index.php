499f88b79d09fc37f47ae2c1b58bc2a901a3f5a7
Dayne <dayne@pilgrimconsulting.com>
2015-11-24 10:10:40 -0600

    updated graph name dynamically.

--- dashboard/index.php
+++ dashboard/index.php
@@ -14,7 +14,7 @@
       </div>
       <?php
         include('selection.php');
-        $top_array = querydb('CALL DASH_main_top('.$_SESSION['game'].');');    
+        $top_array = querydb('CALL DASH_main_top('.$_SESSION['game'].');');
       ?>
     </div>
     <div class="contentpanel">
@@ -168,7 +168,7 @@
         <div class="panel-body">
           <div class="row">
             <div class="col-md-6 mb30">
-              <h5 class="subtitle">Google Installs/Uninstalls</h5>
+              <h5 class="subtitle"><?php echo $_SESSION['game'] = 1 ? 'Google Installs/Uninstalls' : 'Apple Installs/Uninstalls'; ?></h5>
               <div id="line-chart" class="body-chart" style="height: 400px;"></div>
             </div>
             <div class="col-md-6 mb30">
