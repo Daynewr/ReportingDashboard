9512bb90f9261fd6d38ba2ae3f2ad83eaf7c837d
Dayne <dayne@pilgrimconsulting.com>
2015-11-24 13:37:57 -0600

    updated graph heading.

--- dashboard/index.php
+++ dashboard/index.php
@@ -167,7 +167,7 @@
         <div class="panel-body">
           <div class="row">
             <div class="col-md-6 mb30">
-              <h5 class="subtitle"><?php echo ((isset($_SESSION['game']) && $_SESSION['game'] % 2 == 0) ? 'Apple Installs/Uninstalls' : 'Google Installs/Uninstalls'); ?></h5>
+              <h5 class="subtitle"><?php echo (($top_array[0]->game_provider == 'Google Play Store') ? 'Google Installs/Uninstalls':'Apple Installs/Uninstalls'); ?></h5>
               <div id="line-chart" class="body-chart" style="height: 400px;"></div>
             </div>
             <div class="col-md-6 mb30">
